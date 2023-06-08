3d5521a0214201378c878face1c5161796ba9735
Valerii Viun <2455521@gmail.com>
2023-06-08 18:29:34 +0300

    summary added folder components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { ToastContainer } from 'react-toastify';
+import { AppContainer } from 'App.styled';
+import ImageGallery from './ImageGallery';
+import Searchbar from './Searchbar';
+export class App extends Component {
+  state = {
+    query: '',
+  };
+
+  handleFormSubmit = query => {
+    this.setState({ query });
+  };
+
+  render() {
+    const { query } = this.state;
+    return (
+      <AppContainer>
+        <Searchbar onSubmit={this.handleFormSubmit} />
+        <ImageGallery query={query} />
+
+        <ToastContainer autoClose={3000} theme="colored" />
+      </AppContainer>
+    );
+  }
+}
---
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -0,0 +1,44 @@
+import Modal from 'components/Modal';
+import React, { PureComponent } from 'react';
+import { GalleryItem, GalleryItemImage } from './ImageGalleryItem.styled';
+import PropTypes from 'prop-types';
+class ImageGalleryItem extends PureComponent {
+  static propTypes = {
+    id: PropTypes.number.isRequired,
+    webformatURL: PropTypes.string.isRequired,
+    largeImageURL: PropTypes.string.isRequired,
+    tags: PropTypes.string.isRequired,
+  };
+
+  state = {
+    showModal: false,
+  };
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
+  };
+
+  render() {
+    const { showModal } = this.state;
+    const { id, largeImageURL, webformatURL, tags } = this.props;
+    return (
+      <GalleryItem onClick={this.toggleModal}>
+        <GalleryItemImage
+          id={id}
+          src={webformatURL}
+          alt={tags}
+          large={largeImageURL}
+        />
+        {showModal && (
+          <Modal onClose={this.toggleModal}>
+            <img src={largeImageURL} alt={tags} />
+          </Modal>
+        )}
+      </GalleryItem>
+    );
+  }
+}
+
+export default ImageGalleryItem;
